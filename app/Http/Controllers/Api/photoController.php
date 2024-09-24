23049b9dd90de5381ca4144aba0f4ef4c0ddebe2
ma7moud-3adel <mahmoud55048@gmail.com>
2024-09-25 01:50:50 +0300

    updates wz APIs

--- app/Http/Controllers/Api/photoController.php
+++ app/Http/Controllers/Api/photoController.php
@@ -14,23 +14,23 @@ class photoController extends Controller
      * Handle the incoming request.
      */
     
-    // public function __invoke(Request $request, $id)
-    // {
-    //     $photo = ProductImage::where('id',$id)->get();
-    //     if (count($photo)>0){
-    //         return ApiResponse::sendResponse(200,'Product Image Retrieved',PhotoResource::collection($photo));
-    //     }
-    //     return ApiResponse::sendResponse(200,'No Image For This Product',[]);
-
-    // }
-
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, $id)
     {
-        $photo = ProductImage::where('id',$request->input('id'))->get();
+        $photo = ProductImage::where('id',$id)->get();
         if (count($photo)>0){
             return ApiResponse::sendResponse(200,'Product Image Retrieved',PhotoResource::collection($photo));
         }
         return ApiResponse::sendResponse(200,'No Image For This Product',[]);
 
     }
+
+    // public function __invoke(Request $request)
+    // {
+    //     $photo = ProductImage::where('id',$request->input('id'))->get();
+    //     if (count($photo)>0){
+    //         return ApiResponse::sendResponse(200,'Product Image Retrieved',PhotoResource::collection($photo));
+    //     }
+    //     return ApiResponse::sendResponse(200,'No Image For This Product',[]);
+
+    // }
 }
