a98112173f087fd5820ba763e842218ff8da526a
Hossam Elgamal <hossamelgamal13@gmail.com>
2024-09-26 16:48:40 +0300

    ff

--- database/migrations/2024_09_19_095015_create_products_table.php
+++ database/migrations/2024_09_19_095015_create_products_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('details');
-            $table->string('description');
+            $table->text('description');
             $table->string('brand');
             $table->string('movement');
             $table->float('price');
